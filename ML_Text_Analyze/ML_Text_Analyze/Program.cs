b54ff4324ea28e22dbf93d49246539048ed996ad
Luiz Rossa <luizgr20@gmail.com>
2023-10-17 20:34:24 -0300

    Initial commit

---
+++ ML_Text_Analyze/ML_Text_Analyze/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ML_Text_Analyze
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+        }
+    }
+}
